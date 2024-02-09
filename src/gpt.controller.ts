bea5f9d386ea362063bff9d3dbb76b899ff1f9ea
Alejandro Payan <alejandro.payan@sigob.com.mx>
2024-02-08 17:38:16 -0700

    Backend chat

---
+++ src/gpt.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post, Body } from "@nestjs/common";
+import { ChatService } from "./gpt.service";
+
+@Controller('chat')
+export class ChatController {
+    constructor(private readonly chatService: ChatService) {}
+
+    @Post('talk')
+    async talkToGPT(@Body('content') content: string) {
+        return this.chatService.chatWithGPT(content);
+    }
+}
